29cb7149c11b32c1ca43e781a01c54b7a729498a
Jeff Nelson <jpnelson85@gmail.com>
2023-07-14 14:16:23 -0500

    fix test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Items, type: :model do
+RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to(:merchant) }
-    it { should have_many(:invoice_items) }
   end
 
   describe 'validations' do
